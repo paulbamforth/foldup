ee44e2d30b9e44cbdfe5960057afe2611a8fa11f
Paul Bamforth <paulbamforth@hotmail.co.uk>
2015-01-21 19:30:06 +0000

    Added files

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Responsive Website Design</title>
+	<link type="text/css" rel="stylesheet" href="stylesheet.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+	<div id="container">
+		<img src="image.jpg">
+		<ul class="nav">
+			<li><a href="#">Home</a></li>
+			<li><a href="#">About</a></li>
+			<li><a href="#">News</a></li>
+			<li><a href="#">Contact</a></li>
+		</ul>
+		<div id="left-column">This is the left column. We're going to see if we make</div>
+		<div id="right-column">This is the right column. We're going to see if we make</div>
+	</div>
+</body>
+</html>
